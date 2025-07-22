d78ab5b1ca2ae8839ae869946ae33dea5b43778f
Vitor Alves <104380373+Vitorhazael@users.noreply.github.com>
2025-07-22 15:41:36 -0300

    Verificador de Palavras em Strings Python
    Este código permite ao usuário digitar um nome e uma palavra/frase. Ele verifica se a palavra ou frase existe dentro do nome informado e retorna uma mensagem indicando se encontrou ou não. Ótimo para exercitar lógica básica de busca em textos usando Python.

---
+++ Projeto01/Projetov1.py
@@ -0,0 +1,12 @@
+nome = input('Digite seu nome: ')
+encontrar = input('Digite o que deseja encontar: ')
+
+if encontrar in nome:
+   print(f'{encontrar} esta em {nome}')
+
+
+else: 
+   print(f'{encontrar} nao esta em {nome}')
+
+
+   #SERVE PARA VER SE TEM LETRAS OU PALAVARS DENTRO DE NOMES, FRASES E TEXTOS, OK.
